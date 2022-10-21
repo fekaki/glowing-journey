c9d4c38c547fb16b158e426ea267d1554d7b851b
Felipe Arakaki <felipearakaki@outlook.com>
2022-10-21 11:40:36 -0300

    Basic Operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,26 +1,38 @@
-let js = "amazing";
-console.log(40 + 8 + 23 - 10);
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
 
-console.log('Felipe');
-console.log(23);
+// console.log('Felipe');
+// console.log(23);
 
-javascriptIsFun = true;
-console.log(javascriptIsFun);
+// javascriptIsFun = true;
+// console.log(javascriptIsFun);
 
-// console.log(typeof true);
+// // console.log(typeof true);
 
-// console.log(typeof 23);
-// console.log(typeof "Felipe");
-console.log(typeof javascriptIsFun);
+// // console.log(typeof 23);
+// // console.log(typeof "Felipe");
+// console.log(typeof javascriptIsFun);
 
-javascriptIsFun = "YES!";
-console.log(typeof javascriptIsFun);
+// javascriptIsFun = "YES!";
+// console.log(typeof javascriptIsFun);
 
-let year;
-console.log(year);
-console.log(typeof year);
+// let year;
+// console.log(year);
+// console.log(typeof year);
 
-year = 1991;
-console.log(typeof year);
+// year = 1991;
+// console.log(typeof year);
 
-console.log(typeof null);
+// console.log(typeof null);
+
+let age = 30;
+age = 31;
+
+// const birthYear = 1991;
+// birthYear = 1990;
+
+var job = "programmer";
+job = "teacher";
+
+const lastName = "Arakaki";
+console.log(lastName);
