53f2660437a716b6d92aa2743b208120e0e83fd1
Felipe Arakaki <felipearakaki@outlook.com>
2022-10-21 11:09:17 -0300

    lesson 13 Data types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,26 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is fun!");
-40 + 8 + 23 - 10;
-console.log(40+8+23-10);
+console.log(40 + 8 + 23 - 10);
+
+console.log('Felipe');
+console.log(23);
+
+javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+
+// console.log(typeof 23);
+// console.log(typeof "Felipe");
+console.log(typeof javascriptIsFun);
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
