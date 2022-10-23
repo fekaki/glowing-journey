fe03af7cf45e6288d5c047c7c3a365019776eea5
Felipe Arakaki <felipearakaki@outlook.com>
2022-10-23 16:52:26 -0300

    lesson 18 Coding Taking Decisioons: if / else statements

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -113,7 +113,7 @@
 
 //////////////////////////////////////////////
 
-*/
+/*
 const firstName = "Jonas";
 const job = "teacher";
 const birthYear = 1991;
@@ -134,3 +134,24 @@ console.log("String \n\
 multiple \n\
 lines");
 */
+
+//////////////////////////////////////////////
+
+const age = 15;
+
+if(age >= 18) {
+  console.log(`Sarah can start driving licence`);
+} else {
+  const yearsLeft = 18-age;
+  console.log(`Sara is not allowed to drive! Wait ${yearsLeft} years!`)
+};
+
+const birthYear = 2012;
+
+let century
+if(birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+console.log(century);
