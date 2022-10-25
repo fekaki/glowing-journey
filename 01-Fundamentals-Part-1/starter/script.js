8e212ce17a66c28eba41ba52f622071735f0c3df
Felipe Arakaki <felipearakaki@outlook.com>
2022-10-25 17:39:36 -0300

    lesson 20 Type Conversion and Coercion

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -197,3 +197,29 @@ if(markHigherBMI = bmiMark > bmiJonh){
 */
 
 ////////////////////////////////////////////////////
+
+
+// type covvertion
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Felipe'));
+console.log(typeof(NaN));
+
+console.log(String(23), 23);
+
+// type coercion
+
+console.log('I am ' + 23 + ' years old');
+console.log('I am ' + '23' + ' years old');
+console.log('I am ' + String(23) + ' years old');
+
+console.log('23' - '10' - 3);
+console.log('23' + '10' + 3);
+console.log('23' * '2');
+console.log('23' / '2');
+
+let n = '1' + 1; // '11'
+n = n - 1;
+console.log(n);
