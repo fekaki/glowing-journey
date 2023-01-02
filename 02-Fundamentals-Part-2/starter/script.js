b5d0c725802fc83c0befa77f947ae1391ffb9239
Felipe Arakaki <felipearakaki@outlook.com>
2023-01-02 17:19:54 -0300

    lesson 32 Activating Strict Mode

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive :D');
+
+// const interface = 'Audio';
+// const private = 534;
+// const if = 23;
+
+
