148cddd39f06f455645844e3c7d0428aae68033d
Felipe Arakaki <felipearakaki@outlook.com>
2023-01-23 12:12:39 -0300

    lesson 35 Arrow Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -50,7 +50,7 @@ console.log(applOrangeJuice);
 const num = Number("23");
 */
 
-
+/*
 const age1 = calcAge1(1991);
 
 function calcAge1(birthYear) {
@@ -66,3 +66,26 @@ const calcAge2 = function (birthYear) {
 
 const age2 = calcAge2(1991);
 console.log(age2);
+
+*/
+
+/*
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const calcAge3 = (birthYear) => 2037 - birthYear;
+*/
+
+const calcAge3 = (birthYear) => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  // return retirement;
+  return `${firstName} retires in ${retirement} years`;
+};
+
+console.log(yearUntilRetirement(1991, "Felipe"));
