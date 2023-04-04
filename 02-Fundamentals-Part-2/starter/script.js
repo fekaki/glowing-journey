3e09d69c9ebf1a1c319f7f61a2fef215321c381f
Felipe Arakaki <felipearakaki@outlook.com>
2023-04-04 16:16:28 -0300

    lesson 41 Conding Challenge #2

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,16 +1,3 @@
-// Node.js Server
-/*
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-  res.end('Hello from the server');
-});
-
-server.listen(8000, '127.0.0.1', () => {
-  console.log('Listening to requests on port 8000');
-})
-*/
-
 /*
 'use strict';
 
@@ -50,24 +37,20 @@ console.log(applOrangeJuice);
 const num = Number("23");
 */
 
-/*
-const age1 = calcAge1(1991);
+// const age1 = calcAge1(1991);
 
-function calcAge1(birthYear) {
-  return 2037 - birthYear;
-}
+// function calcAge1(birthYear) {
+//   return 2037 - birthYear;
+// }
 
-console.log(age1);
+// console.log(age1);
 
+// const calcAge2 = function (birthYear) {
+//   return 2037 - birthYear;
+// };
 
-const calcAge2 = function (birthYear) {
-  return 2037 - birthYear;
-};
-
-const age2 = calcAge2(1991);
-console.log(age2);
-
-*/
+// const age2 = calcAge2(1991);
+// console.log(age2);
 
 /*
 const calcAge2 = function (birthYear) {
@@ -221,34 +204,70 @@ const ages = [
 console.log(ages);
 */
 
-const friends = ["Michael", "Steven", "Peter"];
+// const friends = ["Michael", "Steven", "Peter"];
 
-// Add elemets
-const newLength = friends.push("Jay");
-console.log(friends);
-console.log(newLength);
+// // Add elemets
+// const newLength = friends.push("Jay");
+// console.log(friends);
+// console.log(newLength);
 
-friends.unshift("John");
-console.log(friends);
+// friends.unshift("John");
+// console.log(friends);
 
-// Remove elements
-friends.pop(); // Last
-const popped = friends.pop();
-console.log(popped);
-console.log(friends);
+// // Remove elements
+// friends.pop(); // Last
+// const popped = friends.pop();
+// console.log(popped);
+// console.log(friends);
 
-friends.shift(); // First
-console.log(friends);
+// friends.shift(); // First
+// console.log(friends);
 
-console.log(friends.indexOf("Steven"));
-console.log(friends.indexOf("Bob"));
+// console.log(friends.indexOf("Steven"));
+// console.log(friends.indexOf("Bob"));
 
-friends.push(23);
-console.log(friends.includes("Steven"));
-console.log(friends.includes("Bob"));
-console.log(friends.includes("23"));
-console.log(friends.includes(23));
+// friends.push(23);
+// console.log(friends.includes("Steven"));
+// console.log(friends.includes("Bob"));
+// console.log(friends.includes("23"));
+// console.log(friends.includes(23));
 
-if (friends.includes("Steven")) {
-  console.log("You have a friend called Steven");
-}
+// if (friends.includes("Steven")) {
+//   console.log("You have a friend called Steven");
+// }
+
+//////////////////////////////////////////////////////////
+// Coding Challenge #2
+
+/*
+Steven is still building his tip calculator, using the same rules as before:
+Tip 15% of the bill if the bill value is between 50 and 300, and if the value is
+different, the tip is 220%
+
+1. Write a function 'calcTip' that takes any bill value as an input and return the
+corresponding tip, calculated based on the rules above (you can check out the code
+from first tip calculator challenge if you need to). Use the function type you like
+most. Test the function using a bill value of 100.
+
+2. And now let's use arrays! So create an array 'bills' containing the test data
+below.
+
+3. Create an array 'tips' containing the tip value for each bill, calculated from
+the function you created before.
+
+4. BONUS: Create an array 'total' containing the total values, so the bill + tip.
+
+TEST DATA: 125,555 and 44
+
+HINT Remember that an array needs a value in each position, and that value can actually be the returned value of a funciotn! So you can just call  a function as array values (so don't store the tip values in separate variavles first, vut right in the new array)
+*/
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(bills, tips, totals);
