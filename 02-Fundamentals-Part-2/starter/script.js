b7acd6e11992ee8cd23ae5524349e145a158d829
Felipe Arakaki <felipearakaki@outlook.com>
2023-04-27 15:09:55 -0300

    lesson 46 Iteration The for Loop

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -409,3 +409,19 @@ TEST DATA: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.9
 //   );
 // }
 
+/////////////////////////////////////////////////////////
+// console.log("Lifting weights repetition 1 🏋️‍♀️");
+// console.log("Lifting weights repetition 2 🏋️‍♀️");
+// console.log("Lifting weights repetition 3 🏋️‍♀️");
+// console.log("Lifting weights repetition 4 🏋️‍♀️");
+// console.log("Lifting weights repetition 5 🏋️‍♀️");
+// console.log("Lifting weights repetition 6 🏋️‍♀️");
+// console.log("Lifting weights repetition 7 🏋️‍♀️");
+// console.log("Lifting weights repetition 8 🏋️‍♀️");
+// console.log("Lifting weights repetition 9 🏋️‍♀️");
+// console.log("Lifting weights repetition 10 🏋️‍♀️");
+
+// for loop keeps running while condition is TRUE
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
+}
